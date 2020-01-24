6566a97c6e0b68198f76783bcf03182bacd54412
Rickard Bolin <ribo1901@student.miun.se>
2020-01-24 13:38:48 +0100

    Small bug fix

--- ex2_gustaf.m
+++ ex2_gustaf.m
@@ -8,16 +8,14 @@ l1 = null(p1');
 l2 = null(p2');
 l3 = null(p3');
 
-
 imshow(church);
 hold on
 rital(l1)
 rital(l2)
 rital(l3)
 
-interline = null([l1'; l2']);
+interline = null([l2'; l3']);
 interline = interline/interline(3);
 scatter(interline(1),interline(2));
 hold off
-d = abs(interline'*l1)/sqrt(l1(1)^2 + l2(2)^2);
-
+d = abs(interline'*l1)/sqrt(l1(1)^2 + l1(2)^2);
