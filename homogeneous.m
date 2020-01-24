f22232fd90c316d0db369e40c1e2ef856090e033
Rickard Bolin <ribo1901@student.miun.se>
2020-01-24 10:02:18 +0100

    Initial commit

---
+++ homogeneous.m
@@ -0,0 +1,7 @@
+coords2D = pflat(x2DCopy);
+coords3D = pflat(x3DCopy);
+
+plot(coords2D(1,:),coords2D(2,:),'.') %Plots a point at (a(1,i),a(2,i)) for  each i.
+figure
+plot3(coords3D(1,:),coords3D(2,:),coords3D(3,:),'.') %Same as above  but 3D.
+axis  equal %Makes  sure  that  all  axes  have  the  same  scale.
