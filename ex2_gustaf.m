39a2142b36b31ab352f352b1925c2cced1709a71
Gustaf Broström <gustaf-96@hotmail.com>
2020-01-24 13:26:16 +0100

    m

---
+++ ex2_gustaf.m
@@ -0,0 +1,23 @@
+close all
+clear
+load compEx2.mat
+
+church = imread('compEx2.jpg');
+
+l1 = null(p1');
+l2 = null(p2');
+l3 = null(p3');
+
+
+imshow(church);
+hold on
+rital(l1)
+rital(l2)
+rital(l3)
+
+interline = null([l1'; l2']);
+interline = interline/interline(3);
+scatter(interline(1),interline(2));
+hold off
+d = abs(interline'*l1)/sqrt(l1(1)^2 + l2(2)^2);
+
