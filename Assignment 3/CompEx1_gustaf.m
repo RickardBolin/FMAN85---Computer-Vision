a31150d16b6b875a3cec26dca994b3638b38e309
Gustaf Broström <gustaf-96@hotmail.com>
2020-02-11 20:51:36 +0100

    m

--- Assignment 3/CompEx1_gustaf.m
+++ Assignment 3/CompEx1_gustaf.m
@@ -1,19 +1,29 @@
 clear
 close all
 load compEx1data.mat
-
-mu = mean(x{1}, 2);
-sigma = std(x{1}');
-
+notnan = isfinite(x{1}(1,:));
+notnan = isfinite(x{2}(1,:)).*notnan;
+x1 = x{1}(:,logical(notnan));
+x2 = x{2}(:,logical(notnan));
+mu = mean(x1, 2);
+sigma = std(x1');
 N1 = [1/sigma(1) 0 -(1/sigma(1))*mu(1);
     0 1/sigma(2) -(1/sigma(2))*mu(2);
     0 0 1];
-
-mu = mean(x{2}, 2);
-sigma = std(x{2}');
-
+x1 = N1*x1;
+mu = mean(x2, 2);
+sigma = std(x2');
 N2 = [1/sigma(1) 0 -(1/sigma(1))*mu(1);
     0 1/sigma(2) -(1/sigma(2))*mu(2);
     0 0 1];
+x2 = N2*x2;
 
-M = 
+M = zeros(length(x1), 9);
+for i = 1:length(x1)
+    
+    M(i,1:3) = x1(1,i)*[1 1 1].*x2(:,i)';
+    M(i,4:6) = x1(2,i)*[1 1 1].*x2(:,i)';
+    M(i,7:9) = x1(3,i)*[1 1 1].*x2(:,i)';
+    
+end
+
