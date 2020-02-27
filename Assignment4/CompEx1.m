e44478cf22b76f04afa8451788a210afaf505935
David <carpen97@gmail.com>
2020-02-27 18:33:30 +0100

    m

--- Assignment4/CompEx1.m
+++ Assignment4/CompEx1.m
@@ -6,23 +6,92 @@ load('compEx1data.mat')
 
 house1 = imread('house1.jpg');
 house2 = imread('house2.jpg');
-
+P1 = P{1}; P2 = P{2};
 X = pflat(X);
 
-rms = @(plane) mean(([plane 1]*[X(1:3,:); ones(1,length(X))]).^2)./(sum(plane(1:3).^2));
-plane = fminsearch(rms, [1 1 1]);
+%% rms fit på vanliga punkter
+figure;
+scatter3(X(1,:),X(2,:),X(3,:));
+hold on;
 
-% sampleSize = 2; % number of points to sample per trial
-% maxDistance = 2; % max allowable distance for inliers
-% 
-% fitFcn = @(points) polyfit(points(:,1),points(:,2),1); % fit function using polyfit
-% evalFcn = ...   % distance evaluation function
-%   @(model, points) sum((points(:, 2) - polyval(model, points(:,1))).^2,2);
-% 
-% [modelRANSAC, inlierIdx] = ransac(points,fitFcn,evalFcn, sampleSize,maxDistance);
+RMS = @(plane) sqrt ( sum (( plane'* X ).^2)/ size (X ,2));
+plane = fminsearch(RMS, [1 1 1 1]');
+RMS(plane) % =  2.1710e-06
 
-% Generate random data for test
-ransac(X,3,1000,0.5,0.5);
+syms x y
+z = -(plane(1)*x+plane(2)*y+plane(4))/plane(3);
+fmesh(z)
+
+%%
+distances = abs (plane' * X);
+histogram(distances,100);
+
+%% ransac
+figure;
+scatter3(X(1,:),X(2,:),X(3,:));
+hold on;
+[plane, n_inliers, bestInlierIdx] = ransac(X,3,1000,0.1,0.5); %n_inliers = 740
+RMS(plane) % = 0.5648
+
+syms x y
+z = -(plane(1)*x+plane(2)*y+plane(4))/plane(3);
+fmesh(z)
+hold on;
+
+%%
+distances = abs (plane' * X);
+histogram(distances,100);
+
+%% rms fit på inliers
+figure;
+scatter3(X(1,:),X(2,:),X(3,:));
+hold on;
+
+inliers = X(:,bestInlierIdx);
+RMS2 = @(plane) sqrt ( sum (( plane'* inliers ).^2)/ size (inliers ,2));
+plane = fminsearch(RMS2, [1 1 1 1]');
+RMS2(plane) % =  2.1710e-06
+
+syms xx yy
+z = -(plane(1)*xx+plane(2)*yy+plane(4))/plane(3);
+fmesh(z)
+hold on;
+
+%%
+distances = abs (plane' * X);
+histogram(distances,100);
+
+%% plotting inliers in pic1
+projected_inliers = P1*inliers;
+projected_inliers = pflat(projected_inliers);
+figure;
+imshow(house1)
+hold on;
+scatter(projected_inliers(1,:),projected_inliers(2,:),'r')
+
+%% plotting inliers in pic2
+projected_inliers = P2*inliers;
+projected_inliers = pflat(projected_inliers);
+figure;
+imshow(house2)
+hold on;
+scatter(projected_inliers(1,:),projected_inliers(2,:),'r')
+
+%% plotting x in pic
+A1 = P1(:,1:3); A2 = P2(:,1:3);
+H = A2/A1;
+x2 = H*x;
+figure;
+subplot(1, 2, 1);
+imshow(house1)
+hold on;
+scatter(x(1,:),x(2,:),'r')
+subplot(1, 2, 2);
+imshow(house2)
+hold on;
+scatter(x2(1,:),x2(2,:),'r')
+
+
 
 
 
